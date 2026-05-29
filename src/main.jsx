a897a04f501df568e5790d9319e500511ce793b2
Otomen1 <116975807+Otomen1@users.noreply.github.com>
2026-05-29 11:38:29 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/react'
 import App from './App.jsx'
+window.__STRAVA_CLIENT_ID = import.meta.env.VITE_STRAVA_CLIENT_ID ?? '';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
